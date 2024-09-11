efef6b32ee1ffa41fb32f89fb821049b61e70944
Mykhailo Parm <mykhailo.parm.dev@gmail.com>
2024-09-11 22:48:56 +0200

    WIP: tasks implementation

--- backend/src/main/java/com/github/mykhailo_parm/taskmanager/controller/TaskController.java
+++ backend/src/main/java/com/github/mykhailo_parm/taskmanager/controller/TaskController.java
@@ -4,11 +4,18 @@ import com.github.mykhailo_parm.taskmanager.dto.TaskDTO;
 import com.github.mykhailo_parm.taskmanager.mapper.Mapper;
 import com.github.mykhailo_parm.taskmanager.model.Task;
 import com.github.mykhailo_parm.taskmanager.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping(path = "api/v1/users/{userId}/tasks")
 public class TaskController {
     private TaskService taskService;
     private Mapper<Task, TaskDTO> taskMapper;
@@ -17,4 +24,18 @@ public class TaskController {
         this.taskService = taskService;
         this.taskMapper = taskMapper;
     }
+
+//    @GetMapping
+//    public List<TaskDTO> getAllTasksOfUser(@PathVariable("userId") Long id) {
+//        List<Task> tasks = taskService.findAll();
+//        return tasks
+//                .stream()
+//                .map(taskMapper::mapTo)
+//                .collect(Collectors.toList());
+//    }
+
+//    @GetMapping(path = "/{taskId}")
+//    public ResponseEntity<TaskDTO> getTaskOfUser(@PathVariable("userId") Long userId, @PathVariable("taskId") Long taskId) {
+//        return taskService.findById()
+//    }
 }
--- backend/src/main/java/com/github/mykhailo_parm/taskmanager/service/TaskService.java
+++ backend/src/main/java/com/github/mykhailo_parm/taskmanager/service/TaskService.java
@@ -1,4 +1,16 @@
 package com.github.mykhailo_parm.taskmanager.service;
 
+import com.github.mykhailo_parm.taskmanager.model.Task;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskService {
+    Task save(Long userId, Task task);
+    Optional<Task> findById(Long userId, Long taskId);
+    List<Task> findAllByUser(Long userId);
+    void delete(Long userId, Long taskId);
+    boolean isExists(Long userId, Long taskId);
+    Task  partialUpdate(Long userId, Long taskId, Task task);
+
 }
